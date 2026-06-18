09e6efc409cdbedc3d25d6a44825c01875e57595
Michel de Carvalho Pires <mpires@MacBook-Air-de-Michel.local>
2026-06-17 21:25:49 -0300

    Initial commit - Structure of project.

---
+++ src/main/java/com/mpires/banksimulation/BankSimulationApplication.java
@@ -0,0 +1,15 @@
+package com.mpires.banksimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSimulationApplication.class, args);
+
+        System.out.println("Hello from bank Simulation.");
+    }
+
+}
